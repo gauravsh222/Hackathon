6abd644b1df0724ca48c371bbb5f3d9b52d59220
gaurav.sharma <gaurav.sharma@D437.blr.velankani.com>
2017-11-06 12:05:42 +0530

    New File

---
+++ Marble_Problem/src/com/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import com.service.Health;
+
+public class Controller {
+
+	public static int findDisapperedBlocks(int[][] arr, String dir, int row, int col) {
+		Traversal t = new Traversal(row, col, arr);
+		while (true) {
+			if ("up".equalsIgnoreCase(dir)){
+				dir = t.up();
+			} else if ("down".equalsIgnoreCase(dir)){
+				dir = t.down();
+			} else if ("left".equalsIgnoreCase(dir)){
+				dir = t.left();
+			} else if ("right".equalsIgnoreCase(dir)){
+				dir = t.right();
+			}
+			else{
+				break;
+			}
+		}
+		return  Health.disAppeared;
+	}
+
+}
