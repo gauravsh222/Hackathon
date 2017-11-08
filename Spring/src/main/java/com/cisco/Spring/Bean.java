56502468716f8b47b3187740f844cf9a56f51a29
gaurav.sharma <gaurav.sharma@D437.blr.velankani.com>
2017-11-08 11:53:00 +0530

    Demo Spring

---
+++ Spring/src/main/java/com/cisco/Spring/Bean.java
@@ -0,0 +1,7 @@
+package com.cisco.Spring;
+
+public interface Bean {
+
+    boolean isABean();
+
+}
---
+++ Spring/src/main/java/com/cisco/Spring/impl/BeanImpl.java
@@ -0,0 +1,11 @@
+package com.cisco.Spring.impl;
+
+import com.cisco.Spring.Bean;
+
+public class BeanImpl implements Bean {
+
+    public boolean isABean() {
+	return true;
+    }
+
+}
